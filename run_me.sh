431bd266664237e6dd0d90cade5e1d1b96f09072
Joshua Hoeflich <josh@joshuahoeflich.com>
2020-09-14 08:36:01 -0500

    Begins integrating pre-commit into project

--- run_me.sh
+++ run_me.sh
@@ -2,13 +2,14 @@
 
 set -e
 
+export CAT_OUTPUT_DIR="$PWD/CAT"
 export CC=clang
 export CXX=clang++
 
 rm -rf build/ ; 
 mkdir build ; 
 cd build ; 
-cmake -DCMAKE_INSTALL_PREFIX="~/CAT" -DCMAKE_BUILD_TYPE=Debug ../ ; 
+cmake -DCMAKE_INSTALL_PREFIX="$CAT_OUTPUT_DIR" -DCMAKE_BUILD_TYPE=Debug ../ ; 
 make ;
 make install ;
 cd ../ 
--- shell.nix
+++ shell.nix
@@ -1,11 +1,18 @@
 { pkgs ? import <nixpkgs> {}}:
 
 pkgs.mkShell {
+  CAT_OUTPUT_DIR = ./. + "/CAT";
   buildInputs = with pkgs; [
     pre-commit
-    llvm
-    clang
+    llvm_9
+    clang_9
     cmake
     shellcheck
   ];
+  shellHook = ''
+    if [ -d "$PWD"/.git ] && [ ! -f "$PWD"/.git/hooks/pre-commit ]; then
+      echo Install pre commit!
+      # pre-commit install
+    fi
+  '';
 }
