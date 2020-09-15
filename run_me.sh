416e4558d947667360d3bbe9e5d2b0b3cc15e414
Joshua Hoeflich <josh@joshuahoeflich.com>
2020-09-15 17:49:43 -0500

    Fixes policy warning in CMake

--- run_me.sh
+++ run_me.sh
@@ -8,7 +8,7 @@ export CXX=clang++
 rm -rf build/ ;
 mkdir build ;
 cd build ;
-cmake -DCMAKE_INSTALL_PREFIX="$CAT_OUTPUT_DIR" -DCMAKE_BUILD_TYPE=Debug ../ ;
+cmake -DCMAKE_POLICY_DEFAULT_CMP0077=NEW -DCMAKE_INSTALL_PREFIX="$CAT_OUTPUT_DIR" -DCMAKE_BUILD_TYPE=Debug ../ ;
 make ;
 make install ;
 cd ../
